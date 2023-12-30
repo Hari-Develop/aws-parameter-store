4c1b43b636a84bcdf42aef85904abfdf23047201
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-12-30 08:17:13 +0530

    commit

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "aws_ssm_parameter" "main" {
   count = length(var.parameter)
   name  = var.parameter[count.index].name
   type  = var.parameter[count.index].type
-  value = var.parameter[count.index].value
+  value = var.parameter[count.index].Value
 }
