416e9f28713312ef4bc3b08ed798bfa9aeb7ea73
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-12-30 13:19:03 +0530

    commit

--- vars.tf
+++ vars.tf
@@ -3,7 +3,7 @@ variable "parameter" {
   default = [
 
     { name = "dev.rds.username", type = "String", Value = "admin1" },
-    { name = "dev.rds.password", type = "SecureString", Value = "ExpenseApp@1" }
+    { name = "dev.rds.password", type = "SecureString", Value = "ExpenseApp121" }
 
   ]
 }
