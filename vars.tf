7c0c36a2f0c82d0d24d2a50ea6dec831febcef25
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2024-01-10 20:58:47 +0530

    commit

--- vars.tf
+++ vars.tf
@@ -3,7 +3,8 @@ variable "parameter" {
   default = [
 
     { name = "dev.rds.username", type = "String", Value = "admin1" },
-    { name = "dev.rds.password", type = "SecureString", Value = "ExpenseApp121" }
+    { name = "dev.rds.password", type = "SecureString", Value = "ExpenseApp121" },
+    { name = "dev.backend.Bakend_end_point", type = "SecureString", Value = "http://backend-dev.unlockers.online/" }
 
   ]
 }
